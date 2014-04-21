5145249f0d593be88e9a29a21b30ec534409627d
Toby Retallick <toby.retallick@gmail.com>
2014-04-21 08:46:00 +0100

    User can post tool listing

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,6 +1,21 @@
 class ToolsController < ApplicationController
 
   def index
+    @tools = Tool.all
+  end
+
+  def new
+    @tool = Tool.new
+  end
+
+  def create
+    @tool = Tool.new params[:tool].permit(:title, :body, :location, :price)
+    
+    if @tool.save
+      redirect_to '/tools'
+    else
+      render 'new'
+    end
   end
   
 end
